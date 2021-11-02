647aae31155c1c92b38f9794c8230cbdc00dae9c
unknown <alejobenjaminpinto@paucasesnovescifp.cat>
2021-11-02 11:34:54 +0100

    Primer commit. - Creación de las clases: 	- MainFrame.java 		- Añadido la JList con la lectura del archivo .json con las obras 		- Añadido la JMenuBar con sus JMenus con los JMenuItems. 		- mnuInsert abre InsertDialog.java 	- Opus.java 	- InsertDialog.java 		- Añadido los JLabels y los JTextFields para la información de las obras. 		- Añadido botones para añdir, limpiar, cancelar y elegir foto. 			- btnAdd añade un nuevo objeto Opus a la lista y copia la imagen seleccionada al destino indicado. 			- btnClear borra los campos y selecciona la imagen por defecto. 		- Label que tendrá como icono la imagen seleccionada ajustada a su tamaño indicado. 	- UpdateDialog.java 	- DeleteDialog.java

---
+++ src/spdvi/Opus.java
@@ -0,0 +1,80 @@
+package spdvi;
+
+/**
+ *
+ * @author Alejo
+ */
+public class Opus {
+    private String registre;
+    private String titol;
+    private int any;
+    private String format;
+    private String autor;
+    private String imatge;
+
+    public Opus() {
+    }
+
+    public Opus(String registre, String titol, int any, String format, String autor, String imagePath) {
+        this.registre = registre;
+        this.titol = titol;
+        this.any = any;
+        this.format = format;
+        this.autor = autor;
+        this.imatge = imagePath;
+    }
+
+    public String getRegistre() {
+        return registre;
+    }
+
+    public void setRegistre(String registre) {
+        this.registre = registre;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int any) {
+        this.any = any;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getImagePath() {
+        return imatge;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imatge = imagePath;
+    }
+
+    @Override
+    public String toString() {
+        return registre + ", " + titol + ", " + any + ", " + format + ", " + autor + ", " + imatge;
+    }
+    
+}
